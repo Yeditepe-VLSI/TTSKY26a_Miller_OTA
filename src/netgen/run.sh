70f5ec350be2d2018e01003c0d2b4985fc253eb3
yusufkulluk <ykulluk67@gmail.com>
2026-05-01 07:31:39 +0300

    fix project name

---
+++ src/netgen/run.sh
@@ -0,0 +1,5 @@
+netgen -batch lvs \
+"../xschem/simulation/miller_ota_wrapper.spice miller_ota_wrapper" \
+"../magic/tt_um_analog_ota_version1.spice tt_um_analog_ota_version1" \
+/usr/local/share/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl \
+comp.out
